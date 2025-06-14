e0bf40f297d8c97d241ab42617b31d66a7c2cb51
Amir-Battal <ameerbatttal@hotmail.com>
2025-06-14 07:42:30 +0300

    deploy

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -2,9 +2,10 @@ import axios from "axios";
 import { ThermometerSun } from "lucide-react";
 
 
-const weather = await axios.get('http://api.weatherapi.com/v1/current.json?key=5019e7881e504e23bb3174509251106&q=Aleppo&aqi=no').then(response => response.data)
 
-const Weather = () => {
+const Weather = async () => {
+  
+  const weather = await axios.get('http://api.weatherapi.com/v1/current.json?key=5019e7881e504e23bb3174509251106&q=Aleppo&aqi=no').then(response => response.data)
 
   return (
     <div className='flex flex-col mr-[2%] items-center'>
